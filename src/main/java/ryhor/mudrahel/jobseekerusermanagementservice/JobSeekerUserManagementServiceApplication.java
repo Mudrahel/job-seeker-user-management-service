879c6c1c450866db79bc917621b839656e4ee433
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2023-06-05 14:20:23 -0400

    Initial commit

---
+++ src/main/java/ryhor/mudrahel/jobseekerusermanagementservice/JobSeekerUserManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package ryhor.mudrahel.jobseekerusermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerUserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekerUserManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ryhor/mudrahel/jobseekerusermanagementservice/JobSeekerUserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ryhor.mudrahel.jobseekerusermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekerUserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
